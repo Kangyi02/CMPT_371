cd6b978316c54dc09813d1a760f7262a2efd1612
Sam Nygard <samnygard@gmail.com>
2024-11-20 12:17:29 -0800

    some comments as a plan

--- Sam/client.py
+++ Sam/client.py
@@ -10,14 +10,15 @@ serverPort = 1200
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 f  = open('test.txt')
 
-
+ssthresh = float('INF')
+dupAckCount = 0
 # Packet gets set up by handshake, for now hard coded
 
 
 
 data = f.read(50)
 while data != '':
-    packet = Packet(0, 1, 1, data)
+    packet = Packet(0, 1, True, data)
     length = clientSocket.sendto(packet.change_to_bytes(),(serverName, serverPort))
     print('length of message sent:', length, 'len of message:', len(data))
     modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
@@ -25,4 +26,8 @@ while data != '':
     data = f.read(50)
 
 
-    # send
+    # send packet
+    # set timer for correct ack number
+    # if recieve other ack or time out start resending from that point. and set 
+    # when you recieve send two more packets
+ 
